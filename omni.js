0c9b7b73c19315efdc84bc3fa9b77efb84d96db0
arihant518 <arihanttongya123@gmail.com>
2022-08-26 12:51:20 -0700

    final commit

---
+++ omni.js
@@ -0,0 +1,13 @@
+// const express = require('express');
+// const app = express();
+//
+// app.get("/",function (req,res) {
+//   res.sendFile(__dirname + "/index.html")
+// })
+//
+// app.use(express.static("public"));
+//
+//
+// app.listen(3000,function () {
+//   console.log("server started at port 3000");
+// })
